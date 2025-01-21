c4b2d9c5449d0e1ad07ff1dd971b2fd2530777d8
JulioVS <julioer@me.com>
2025-01-21 14:47:40 -0300

    Add initial blog entries and display logic

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+var entries = [
+  {
+    title: "The Art of Self Preservation",
+    content:
+      "Boundaries are not just lines drawn to separate us from external pressures; they are lifelines that safeguard our well-being in the realm of EDI work. Belonging to a marginalized community means our experiences are deeply intertwined with our identities. It is crucial to define and communicate our boundaries clearly, both with ourselves and with those around us.",
+    author: "Julio",
+    date: "December 1st, 2024",
+  },
+  {
+    title: "Is 90125 really a Yes album?",
+    content:
+      "90125 is the secret word required to return to the present day from 1983 in Jeb Wright's Blast From the Past. That's just one of the classic rock connections in this delightfully funny new novel.",
+    author: "Julio",
+    date: "January 5th, 2025",
+  },
+  {
+    title: "Fast cars are a dying breed",
+    content:
+      "A few days ago, I had the opportunity to drive a high performance European sports car with its naturally aspirated engine generating 450 HP under the hood. A car that is highly responsive to the driver’s commands and is a sheer driving pleasure. Yet with all its driving pleasure and beauty I see this and similar cars as a dying breed. Not because these are not wonderful cars, but because of the paradigm shift that self-driving and electric cars are creating.",
+    author: "Julio",
+    date: "January 15th, 2025",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { blog: entries });
 });
 
 app.listen(port, () => {
